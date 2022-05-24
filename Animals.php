a533eb79d1e19f04dd2f8e97b1bb128bb37089ca
ascaris29 <solenelema2009@hotmail.com>
2022-05-24 18:47:14 +0200

    second commit

---
+++ Animals.php
@@ -0,0 +1,15 @@
+<?php 
+
+class Animals {
+
+    public $head;
+    public $fur;
+    public $paw;
+    public $tail;
+    public $typeOfCry
+    public function string cry(){
+        return 
+    }
+
+
+}
