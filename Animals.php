845718542ebe94fe9e83c9fe6fc75d7d81dd16b5
ascaris29 <solenelema2009@hotmail.com>
2022-05-24 20:03:57 +0200

    third commit

--- Animals.php
+++ Animals.php
@@ -1,15 +1,69 @@
-<?php 
+<?php
 
-class Animals {
+declare(strict_types=1);
 
-    public $head;
-    public $fur;
-    public $paw;
-    public $tail;
-    public $typeOfCry
-    public function string cry(){
-        return 
+class Animals{
+
+    public $typeOfCry;
+    public $typeOfDeplacement;
+    public $typeOfAlimentation;
+    public $gender;
+
+    public function Gender($gender){
+        if(gender === 1){
+
+        }
+    }
+    public function cry(){
+        echo "";
     }
 
+    public function eat(){
+        echo "";
+    }   
+
+    public function moving(){
+        echo"";
+    }
+}
+
+class Cat extends Animals{
+
+    public $typeOfCry = "miaou";
+    public $typeOfDeplacement ="walk";
+    public $typeOfAlimentation = "carnivorous";
+    public $name;
+    public $color;
+
+
+    public function cry(){
+        echo $this->typeOfCry;
+    }
+    public function eat(){
+        echo $this->typeOfAlimentation;
+    }
 
 }
+
+class Dog extends Animals{
+
+    public $typeOfCry = "waf";
+    public $typeOfDeplacement ="walk";
+    public $typeOfAlimentation = "carnivorous";
+    public $name;
+    public $color;
+
+    public function cry(){
+        echo $this->typeOfCry;
+    }
+    public function eat(){
+        echo $this->typeOfAlimentation;
+    }
+
+}
+
+$minette = new Cat;
+$minette->cry;
+$minette->name = "minette";
+
+var_dump($minette);
